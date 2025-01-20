bcc06257e5d9854a41207702fc0cead940961eaa
Sik-Wa <sikwavivian@gmail.com>
2025-01-20 20:40:45 +0200

    stop-watch

--- src/components/beginner-projects/Stop-watch.jsx
+++ src/components/beginner-projects/Stop-watch.jsx
@@ -38,14 +38,22 @@ setElapsedtime((prev)=> prev + 10 )
   }
 
 
+  const formatTime = (time)=>{
+    const minutes = Math.floor(time/ 60000);
+      const seconds = Math.floor((time % 60000) / 1000);
+      const milliseconds = Math.floor((time % 1000) / 10);
+
+      return `${minutes}:${seconds < 10 ? '0': ''}${seconds}.${milliseconds< 10 ? '0': ''}${milliseconds}`
+}
+
   return (
     <div className=" flex items-center justify-center my-52">
       <div className=" container">
       <h1 className=" text-center font-bold text-xl mb-6">Stop-Watch</h1>
-      <h1 className=" text-center font-bold text-xl mt-4 mb-6">{elapsedtime}</h1>
+      <h1 className=" text-center font-bold text-xl mt-4 mb-6">{formatTime(elapsedtime)}</h1>
         <div className=" flex justify-center space-x-10 text-lg font-bold">
             <button onClick={startStopWatch}  disabled={running} className="">start</button>
-            <button onClick={stopStopWatch} disabled={running} className="">stop</button>
+            <button onClick={stopStopWatch} disabled={!running} className="">stop</button>
             <button onClick={resetStopWatch} className="">reset</button>
         </div>
       </div>
